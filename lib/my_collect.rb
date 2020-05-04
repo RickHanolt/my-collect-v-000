155e653aff6e976867941b07a49a0b057dacafe7
Richard Hanolt <rhanolt@gmail.com>
2020-05-04 23:29:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(collection)
   i = 0
   updated_collection = []
-  while i < array.length
-    updated_collection << (yield array[i])
+  while i < collection.length
+    updated_collection << (yield collection[i])
     i += 1
   end
 updated_collection
